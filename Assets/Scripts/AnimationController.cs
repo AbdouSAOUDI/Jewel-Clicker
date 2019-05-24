10f86ca9044be8d40d387372f0a3f9aae81b4599
Star-PRO-X <Abderrahmane.saoudi.26@gmail.com>
2019-05-24 09:05:37 +0100

    Version 0.1

---
+++ Assets/Scripts/AnimationController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    [SerializeField] private Animator jewelAnim, coinAnim;
+    public void JewelAnimationChange(int status)
+    {
+        if (status == 0) jewelAnim.SetBool("JewelClicked", false);
+        else jewelAnim.SetBool("JewelClicked", true);
+        Debug.Log("Jewel Animation Change !");
+    }
+    public void CoinAnimationChange(int status)
+    {
+        if (status == 0) coinAnim.SetBool("CoinClicked", false);
+        else coinAnim.SetBool("CoinClicked", true);
+        Debug.Log("CoinAnimationChange !");
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] private Text scoreT;
+    [SerializeField] private int score = 0, js = 1, p;
+    [SerializeField] private AnimationController animCntrlr;
+    
+    public void JewelClick(Text scoreText)
+    {
+        score += js;
+        scoreText.text = score.ToString();
+        animCntrlr.JewelAnimationChange(1);
+        animCntrlr.CoinAnimationChange(1);
+        //Debug.Log("Completed !");
+    }
+    
+    public void Back(int sceneNumber)
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+    public void ExitGame()
+    {
+        Debug.Log("Game Closed!");
+        Application.Quit();
+    }
+    public void BuyItem(int jewelPerSecond)
+    {
+        //if (score > jewelPerSecond)
+        //{
+        //    p += jewelPerSecond;
+        //    score -= js;
+        //    scoreT.text = score.ToString();
+        //}
+        p += jewelPerSecond;
+    }
+    public void DecreaseScore(int Price)
+    {
+        if (score > Price)
+        {
+            js += p;
+            score -= Price;
+            scoreT.text = score.ToString();
+        }
+    }
+}
